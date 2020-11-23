7d0f8f4a526067e34d25b092d591bebf6da662cc
soy-A <skql1502@gmail.com>
2020-11-23 19:55:58 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,9 @@
-import java.awt.Graphics;
-import java.awt.Image;
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
 
-public class Main extends JFrame{
-	static JPanel page1 = new JPanel() {
-		Image background = new ImageIcon(Main.class.getResource("/Users/soy-a/Downloads/baekgu.jpeg")).getImage();
-		public void paint(Graphics g) {
-			g.drawImage(background, 0, 0, null);
-		}
-	};
-	public Main() {
-		homeframe();
-	}
-	public void homeframe() {
-		setTitle("testing");
-		setSize(400,600);
-		setResizable(false);
-		setLocationRelativeTo(null);
-		setLayout(null);
-		setVisible(true);
-		page1.setLayout(null);
-		page1.setBounds(0,0,400,600);
-		add(page1);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
-
+public class Main{
+	
 	public static void main(String[] args) {
-		new Main();
-
+		Folder newfolder = new Folder("album");
+		String path = newfolder.makedir();
 	}
 
 }
