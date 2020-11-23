bd07316c3aa13e5a7e767418cc7ed0c2878ca04d
soy-A <skql1502@gmail.com>
2020-11-24 01:13:48 +0900

    feat: Upload AddToAlbum.java
    1. 사용자가 선택한 파일들을 album폴더에 복사하는 AddToAlbum 클래스를 추가함

---
+++ src/AddToAlbum.java
@@ -0,0 +1,49 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class AddToAlbum {
+	
+	public static void addToAlbum(File[] targetFiles, String albumPath) {	// 선택한 파일들은 파일리스트에 넣어둘 것
+		
+		for(File file : targetFiles) {
+			
+			File temp = new File(albumPath + File.separator + file.getName());
+			FileInputStream fis = null;
+			FileOutputStream fos = null;
+			
+			try {
+				
+				fis = new FileInputStream(file);
+				fos = new FileOutputStream(temp);
+				byte[] buf = new byte[4096];
+				int cnt = 0;
+				
+				while((cnt = fis.read(buf)) != -1) {
+					
+					fos.write(buf, 0, cnt);
+					
+				}
+				
+			} catch (Exception e) {
+				
+				e.printStackTrace();
+				
+			} finally {
+				
+				try {
+					
+					fis.close();
+					fos.close();
+					
+				} catch (IOException e) {
+					
+					e.printStackTrace();
+					
+				}
+			}
+		}
+	}
+	
+}
