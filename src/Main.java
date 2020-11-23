06dfecb447a34fd1b88c9070cac9a22ba470f5d6
soy-A <skql1502@gmail.com>
2020-11-23 21:53:15 +0900

    feat: Add userPath and photoList
    1. 사용자에게서 이미지를 읽어올 폴더의 주소를 입력받는 기능 추가.
    2. 사용자 폴더 주소에서 이미지 파일들만 가져와 리스트를 생성하는 기능 추가.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,22 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class Main{
 	
 	public static void main(String[] args) {
-		Folder newfolder = new Folder("album");
-		String path = newfolder.makedir();
+		
+		/* 앨범으로 사용할 폴더를 생성 */
+		Folder folder = new Folder("album");
+		String albumPath = folder.makedir();
+		
+		/* 이미지가 저장된 폴더의 경로를 입력받는다 */
+		Scanner scan = new Scanner(System.in);
+		String userPath = scan.next();
+		
+		/* 사진 리스트 생성(파일명, 년, 월, 일 포함) */
+		ArrayList<HashMap<String, Object>> photoList = new ArrayList<HashMap<String, Object>>();
+		photoList = GetPhoto.getPhotoList(userPath);
 	}
 
 }
