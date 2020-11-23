675e03767b8b37f81eb364d07b4c55ac5c2471f2
soy-A <skql1502@gmail.com>
2020-11-23 21:35:45 +0900

    feat: Upload GetPhoto.java
    1. 경로 내 파일들의 확장자를 확인, 이미지 파일의 이름, 년, 월 일을 리스트에 저장하는 GetPhoto클래스를 추가함.

---
+++ src/GetPhoto.java
@@ -0,0 +1,49 @@
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+
+public class GetPhoto {
+	
+	private static String image_extension = "jpg,jpeg,png,gif,swf,bmp";	// 이미지 파일 확장자
+	
+	public static ArrayList<HashMap<String, Object>> getPhotoList(String userPath){
+		
+		ArrayList<HashMap<String, Object>> photoList = new ArrayList<HashMap<String, Object>>();
+		File userFile = new File(userPath);
+		
+		if(userFile.exists() && userFile.isDirectory()) {
+			
+			File[] fileList = userFile.listFiles();
+			
+			for (File tempFile : fileList) {
+				
+				if(tempFile.isFile() && tempFile.length() > 0) {
+					
+					/* 파일 이름, 년, 월, 일을 리스트에 저장 */
+					String fileFullName = tempFile.getName();
+					String onlyFileExt = (fileFullName.substring(fileFullName.lastIndexOf(".")+1)).toLowerCase();
+					String onlyFileName = fileFullName.toLowerCase().substring(0, fileFullName.lastIndexOf("."));
+					
+					SimpleDateFormat year = new SimpleDateFormat("yyyy");
+					SimpleDateFormat month = new SimpleDateFormat("MM");
+					SimpleDateFormat day = new SimpleDateFormat("dd");
+					Date fileDate = new Date(tempFile.lastModified());
+					
+					if(image_extension.contains(onlyFileExt)) {	// 확장자 확인
+						
+						HashMap<String, Object> photo = new HashMap<String, Object>();
+						photo.put("filename", onlyFileName);
+						photo.put("fileyear", year.format(fileDate));
+						photo.put("filemonth", month.format(fileDate));
+						photo.put("fileday", day.format(fileDate));
+						
+						photoList.add(photo);
+					}
+				}
+			}
+		}
+		return photoList;
+	}
+}
