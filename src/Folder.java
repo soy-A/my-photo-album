1c89d781b1538dcc9b1c9bc60d1b5a4f87b1d3e8
soy-A <skql1502@gmail.com>
2020-11-23 21:36:53 +0900

    style: Change line break and comment

--- src/Folder.java
+++ src/Folder.java
@@ -5,14 +5,16 @@ public class Folder {
 	
 	private String dirname;
 	
-	// 생성자
 	public Folder(String dirname) { this.dirname = dirname; }
 	
-	// 파일을 관리할 폴더 만듦
+	/* 파일을 관리할 폴더 만듦 */
 	public String makedir() {
+		
 		final String path = System.getProperty("user.dir") + "/" + dirname;
 		File folder = new File(path);
 		folder.mkdir();
 		return path;
+		
 	}
+	
 }
