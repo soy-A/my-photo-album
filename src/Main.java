3eb636d1e58d7cb3dcd05bc39cbd5ba0cab26f0c
soy-A <skql1502@gmail.com>
2020-11-26 06:43:06 +0900

    style: Update Main.java
    1. albumPath 변수를 static으로 변경함
    2. 대부분의 함수를 GUI에서 실행하도록 옮김

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,18 @@
-import java.io.File;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Scanner;
 
 public class Main{
+	
+	static String albumPath;
 	
 	public static void main(String[] args) {
 		
 		/* 앨범으로 사용할 폴더를 생성 */
 		Folder folder = new Folder("album");
-		String albumPath = folder.makedir();
-		
-		/* 이미지가 저장된 폴더의 경로를 입력받는다 */
-		Scanner scan = new Scanner(System.in);
-		String userPath = scan.next();
-		
-		/* 사진 리스트 생성(파일명, 년, 월, 일 포함) */
-		ArrayList<HashMap<String, Object>> photoList = new ArrayList<HashMap<String, Object>>();
-		photoList = GetPhoto.getPhotoList(userPath);
-		
-		/* test용도로 File[]만듦 - 수정할 것 */
-		File[] tests = new File[1];
-		int temp = 0;
-		for(HashMap<String, Object> i : photoList) {
-			File test = new File(userPath + File.separator + i.get("filefullname"));
-			tests[temp] = test;
-			temp++;
-		}
-
-		/* 선택한 파일을 앨범 폴더에 추가 */
-		Album.addToAlbum(tests, albumPath);
+		albumPath = folder.makedir();
 		
 		/* 선택한 파일을 앨범 폴더에서 삭제 */
 //		Album.deleteFromAlbum(tests[0], albumPath);
 		
+		/* GUI */
 		GUI gui = new GUI();
 		gui.setVisible(true);
 	}
