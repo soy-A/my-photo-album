1ed78ed8acbdc0ef9694da722df9aa6b4a419c84
soy-A <skql1502@gmail.com>
2020-12-04 03:20:07 +0900

    update: Change the top bar colour to light gray
    - 상단바 색상 변경(gray -> light gray)

--- src/PathPanel.java
+++ src/PathPanel.java
@@ -20,73 +20,74 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 
-public class PathPanel extends JPanel{
+public class PathPanel extends JPanel {
 
 	private JTextField path_text;
-	
+
 	/* 아래 두개는 스크롤 문제를 해결하지 못할 시 다시 코드 안쪽 (final)로 되돌려놓을 것 */
 	private int height;
 	private JScrollPane scrollPane;
 	private static ArrayList<HashMap<String, Object>> photoList = new ArrayList<HashMap<String, Object>>();
-	
+
 	/* <사진 가져오기> 패널 */
 	public PathPanel() {
 
 		setLayout(new BorderLayout(0, 0));
-		
+
 		/* <사진 가져오기 - 경로 입력> 패널 */
 		JPanel pathget_panel = new JPanel();
+		pathget_panel.setBackground(Color.LIGHT_GRAY);
 		add(pathget_panel, BorderLayout.NORTH);
-		
+
 		JLabel path_label = new JLabel("사진을 불러올 폴더의 경로를 입력해주세요");
 		pathget_panel.add(path_label);
-		
+
 		path_text = new JTextField();
 		pathget_panel.add(path_text);
 		path_text.setColumns(30);
-		
+
 		JButton pathget_button = new JButton("불러오기");
 		pathget_button.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				
-				String userPath = path_text.getText();	// 입력받은 경로
-				
-				/* 사진 리스트 생성(파일명, 년, 월, 일 포함) */			
+
+				String userPath = path_text.getText(); // 입력받은 경로
+
+				/* 사진 리스트 생성(파일명, 년, 월, 일 포함) */
 				photoList = GetPhoto.getPhotoList(userPath);
-				
+
 				/* <사진 가져오기 - 메인> 패널 */
 				JPanel image_panel = new JPanel();
 				height = image_panel.getHeight();
 //				Dimension panel_size = new Dimension(400, height);
-				Dimension panel_size = new Dimension(400, 2000);	// 임시로 2000의 값을 주었다(스크롤이 생성되지 않는 문제)
+				Dimension panel_size = new Dimension(400, 2000); // 임시로 2000의 값을 주었다(스크롤이 생성되지 않는 문제)
 				image_panel.setPreferredSize(panel_size);
 				image_panel.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 5));
-				scrollPane = new JScrollPane(image_panel, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+				scrollPane = new JScrollPane(image_panel, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+						JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 				add(scrollPane, BorderLayout.CENTER);
 				scrollPane.setViewportView(image_panel);
-				
+
 				/* 체크박스(사진 선택) 생성, 선택한 사진들을 앨범 폴더에 추가 */
 				JCheckBox photo_check[] = new JCheckBox[photoList.size()];
 				ImageIcon photo_icon[] = new ImageIcon[photoList.size()];
 				Dimension icon_size = new Dimension(145, 145);
 				ArrayList<Integer> selected_index = new ArrayList<Integer>();
-				
-				class PhotoItem implements ItemListener{
+
+				class PhotoItem implements ItemListener {
 
 					/* 선택된 사진을 표시하며, 선택된 인덱스를 저장 */
 					@Override
 					public void itemStateChanged(ItemEvent e) {
-						for(int i = 0; i < photoList.size(); i++) {
-							if(e.getItem() == photo_check[i]) {
-								if(e.getStateChange() == ItemEvent.SELECTED) {
-									photo_check[i].setOpaque(true);		// 선택 표시
+						for (int i = 0; i < photoList.size(); i++) {
+							if (e.getItem() == photo_check[i]) {
+								if (e.getStateChange() == ItemEvent.SELECTED) {
+									photo_check[i].setOpaque(true); // 선택 표시
 									selected_index.add(i);
-								}
-								else if(e.getStateChange() == ItemEvent.DESELECTED) {
+								} else if (e.getStateChange() == ItemEvent.DESELECTED) {
 									photo_check[i].setOpaque(false);
-									for(int j = 0; j < selected_index.size(); j++) {
-										if(selected_index.get(j) == i) {
+									for (int j = 0; j < selected_index.size(); j++) {
+										if (selected_index.get(j) == i) {
 											selected_index.remove(j);
 										}
 									}
@@ -95,20 +96,24 @@ public class PathPanel extends JPanel{
 						}
 					}
 				}
-				
-				for(int i = 0; i < photoList.size(); i++) {
+
+				for (int i = 0; i < photoList.size(); i++) {
 					photo_check[i] = new JCheckBox();
 					photo_check[i].addItemListener(new PhotoItem());
 					photo_check[i].setBackground(Color.DARK_GRAY);
-					photo_icon[i] = new ImageIcon(userPath + File.separator +photoList.get(i).get("filefullname"));
+					photo_icon[i] = new ImageIcon(userPath + File.separator + photoList.get(i).get("filefullname"));
 					Image img = photo_icon[i].getImage().getScaledInstance(135, 135, Image.SCALE_SMOOTH);
 					photo_icon[i] = new ImageIcon(img);
 					photo_check[i].setIcon(photo_icon[i]);
 					photo_check[i].setPreferredSize(icon_size);
 					image_panel.add(photo_check[i]);
 				}
-				/* 이 위까지는 패널의 height가 0임, 그러나 한번 렌더링되면 height가 제대로 들어옴. 따라서 아래의 componentlistener를 잘 활용해 이 값을 가질 수 있도록 시도해볼 것 
-				 * https://stackoverflow.com/questions/8336262/why-cant-i-access-my-panels-getwidth-and-getheight-functions 를 참고 */
+				/*
+				 * 이 위까지는 패널의 height가 0임, 그러나 한번 렌더링되면 height가 제대로 들어옴. 따라서 아래의
+				 * componentlistener를 잘 활용해 이 값을 가질 수 있도록 시도해볼 것
+				 * https://stackoverflow.com/questions/8336262/why-cant-i-access-my-panels-
+				 * getwidth-and-getheight-functions 를 참고
+				 */
 //				image_panel.addComponentListener(new ComponentAdapter() {
 //					public void componentResized(ComponentEvent e) {
 //						height = image_panel.getHeight();
@@ -120,17 +125,17 @@ public class PathPanel extends JPanel{
 //						image_panel.requestFocusInWindow();
 //					}
 //				});
-				
+
 				/* 경로 입력 패널 숨기기 */
 				pathget_panel.setVisible(false);
-				
+
 				/* <사진 가져오기 - 경로 입력 - 추가> 패널 */
 				JPanel add_panel = new JPanel();
 				add(add_panel, BorderLayout.NORTH);
-				
+
 				JLabel add_label = new JLabel("앨범에 추가할 사진을 선택한 후 \"추가\" 버튼을 눌러주세요");
 				add_panel.add(add_label);
-				
+
 				JButton add_button = new JButton("추가");
 				add_button.addActionListener(new ActionListener() {
 
@@ -138,20 +143,21 @@ public class PathPanel extends JPanel{
 					public void actionPerformed(ActionEvent e) {
 						File selected_photo[] = new File[selected_index.size()];
 						ArrayList<HashMap<String, Object>> selected_list = new ArrayList<HashMap<String, Object>>();
-						
-						for(int i = 0; i < selected_index.size(); i++) {
-							selected_photo[i] = new File(userPath + File.separator + photoList.get(selected_index.get(i)).get("filefullname"));
+
+						for (int i = 0; i < selected_index.size(); i++) {
+							selected_photo[i] = new File(userPath + File.separator
+									+ photoList.get(selected_index.get(i)).get("filefullname"));
 							selected_list.add(i, photoList.get(selected_index.get(i)));
 						}
 						Album.addToAlbum(selected_photo, Main.albumPath);
 						Key.addToKey(selected_list, "Key");
 						JOptionPane.showMessageDialog(null, "사진이 추가되었습니다.");
 					}
-					
+
 				});
 				add_panel.add(add_button);
 				add_panel.setVisible(true);
-				
+
 			}
 		});
 		pathget_panel.add(pathget_button);
--- src/PhotoPanel.java
+++ src/PhotoPanel.java
@@ -19,80 +19,80 @@ import javax.swing.JTextArea;
 import javax.swing.SwingConstants;
 
 public class PhotoPanel extends JPanel {
-	
+
 	/* 선택한 이미지의 크기를 패널의 크기에 맞게 조정 */
 	private BufferedImage resizing(Image selected_image, double panel_width, double panel_height) {
-		
+
 		BufferedImage resized_bufimage;
 		Image resized_image;
-		
-		//	비율 계산
+
+		// 비율 계산
 		double panel_ratio = panel_width / panel_height;
-		double image_width = (double)selected_image.getWidth(null);
-		double image_height = (double)selected_image.getHeight(null);
+		double image_width = (double) selected_image.getWidth(null);
+		double image_height = (double) selected_image.getHeight(null);
 		double image_ratio = image_width / image_height;
-		
+
 		if (image_ratio <= panel_ratio) {
 			image_height = panel_height;
 			image_width = panel_height * image_ratio;
-			resized_image = selected_image.getScaledInstance((int)image_width, (int)image_height, Image.SCALE_SMOOTH);
-			resized_bufimage = new BufferedImage((int)image_width, (int)image_height, BufferedImage.TYPE_INT_RGB);
+			resized_image = selected_image.getScaledInstance((int) image_width, (int) image_height, Image.SCALE_SMOOTH);
+			resized_bufimage = new BufferedImage((int) image_width, (int) image_height, BufferedImage.TYPE_INT_RGB);
 			Graphics g = resized_bufimage.getGraphics();
 			g.drawImage(resized_image, 0, 0, null);
 			g.dispose();
-			
+
 		} else {
-			image_width = (int)panel_width;
-			image_height = (int)(panel_width / image_ratio);
-			resized_image = selected_image.getScaledInstance((int)image_width, (int)image_height, Image.SCALE_SMOOTH);
-			resized_bufimage = new BufferedImage((int)image_width, (int)image_height, BufferedImage.TYPE_INT_RGB);
+			image_width = (int) panel_width;
+			image_height = (int) (panel_width / image_ratio);
+			resized_image = selected_image.getScaledInstance((int) image_width, (int) image_height, Image.SCALE_SMOOTH);
+			resized_bufimage = new BufferedImage((int) image_width, (int) image_height, BufferedImage.TYPE_INT_RGB);
 			Graphics g = resized_bufimage.getGraphics();
 			g.drawImage(resized_image, 0, 0, null);
 			g.dispose();
 		}
-		
+
 		return resized_bufimage;
-		
+
 	}
-	
+
 	public PhotoPanel(Image selected_image, ArrayList<HashMap<String, Object>> photoList, int i) {
-		
+
 		setLayout(new BorderLayout(0, 0));
-		
+
 		/* 큰 사진을 띄우는 패널 */
 		JPanel big_image_panel = new JPanel();
-		
+
 		JLabel resized_label = new JLabel(new ImageIcon(resizing(selected_image, 760, 500)));
 
 		big_image_panel.add(resized_label);
 
 		add(big_image_panel, BorderLayout.CENTER);
-		
+
 		/* 사진 위에 띄울 정보 패널 */
 		JPanel info_panel = new JPanel();
-		info_panel.setBackground(Color.GRAY);
+		info_panel.setBackground(Color.LIGHT_GRAY);
 		add(info_panel, BorderLayout.NORTH);
 		info_panel.setLayout(new BorderLayout(0, 0));
-		
+
 		JPanel east_panel = new JPanel();
-		east_panel.setBackground(Color.GRAY);
+		east_panel.setBackground(Color.LIGHT_GRAY);
 		info_panel.add(east_panel, BorderLayout.EAST);
-		
+
 		JPanel west_panel = new JPanel();
-		west_panel.setBackground(Color.GRAY);
+		west_panel.setBackground(Color.LIGHT_GRAY);
 		info_panel.add(west_panel, BorderLayout.WEST);
-		
+
 		/* 사진의 날짜 */
-		JLabel date_label = new JLabel((String)photoList.get(i).get("filedatetime"));
+		JLabel date_label = new JLabel((String) photoList.get(i).get("filedatetime"));
 		date_label.setHorizontalAlignment(SwingConstants.CENTER);
 		info_panel.add(date_label, BorderLayout.CENTER);
-		
+
 		/* 좋아요 버튼을 누른 사진들은 '좋아하는 사진'탭에서 확인할 수 있다 */
 		JButton like_button = new JButton("♥");
 		like_button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				ArrayList<HashMap<String, Object>> likedList = Key.bringKeys("Like");
-				if(likedList.contains(photoList.get(i))) {
+				if (likedList.contains(photoList.get(i))) {
 					Key.deleteKey(likedList, photoList.get(i), "Like");
 					JOptionPane.showMessageDialog(null, "좋아하는 사진에서 삭제되었습니다.");
 				} else {
@@ -103,7 +103,7 @@ public class PhotoPanel extends JPanel {
 			}
 		});
 		east_panel.add(like_button);
-		
+
 		/* 메모 버튼을 누르면 저장된 메모를 보여주며, 메모를 할 수 있는 창을 띄운다 */
 		JButton memo_button = new JButton("메모");
 		memo_button.addActionListener(new ActionListener() {
@@ -115,33 +115,34 @@ public class PhotoPanel extends JPanel {
 				JPanel memo_panel = new JPanel();
 				big_image_panel.add(memo_panel, BorderLayout.EAST);
 				memo_panel.setLayout(new BorderLayout(0, 0));
-				
+
 				JPanel memo_buttons_panel = new JPanel();
 				memo_panel.add(memo_buttons_panel, BorderLayout.NORTH);
-				
+
 				JScrollPane memo_scroll = new JScrollPane();
 				memo_panel.add(memo_scroll, BorderLayout.CENTER);
-				
+
 				JTextArea memo_textArea = new JTextArea(20, 7);
-				File temp = new File(Album.memoPath + File.separator + (String)photoList.get(i).get("filename") + ".txt");
-				if(temp.exists()) {
-					Album.getSavedMemo((String)photoList.get(i).get("filename"), memo_textArea);
+				File temp = new File(
+						Album.memoPath + File.separator + (String) photoList.get(i).get("filename") + ".txt");
+				if (temp.exists()) {
+					Album.getSavedMemo((String) photoList.get(i).get("filename"), memo_textArea);
 				}
 				memo_scroll.setViewportView(memo_textArea);
-				
+
 				JButton memo_save_button = new JButton("저장");
 				memo_save_button.addActionListener(new ActionListener() {
 
 					@Override
 					public void actionPerformed(ActionEvent e) {
 						String getText = memo_textArea.getText();
-						Album.addToMemo((String)photoList.get(i).get("filename"), getText);
+						Album.addToMemo((String) photoList.get(i).get("filename"), getText);
 						JOptionPane.showMessageDialog(null, "메모가 저장되었습니다.");
 					}
-					
+
 				});
 				memo_buttons_panel.add(memo_save_button);
-				
+
 				JButton memo_close_button = new JButton("닫기");
 				memo_save_button.addActionListener(new ActionListener() {
 
@@ -149,7 +150,7 @@ public class PhotoPanel extends JPanel {
 					public void actionPerformed(ActionEvent e) {
 
 					}
-					
+
 				});
 				memo_buttons_panel.add(memo_close_button);
 
@@ -157,15 +158,16 @@ public class PhotoPanel extends JPanel {
 			}
 		});
 		east_panel.add(memo_button);
-		
+
 		/* 현재 보고있는 사진을 삭제함 */
 		JButton delete_button = new JButton("삭제");
 		delete_button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				int delete = JOptionPane.showConfirmDialog(null, "정말로 삭제하시겠습니까?", "삭제", JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE);
-				if(delete == JOptionPane.YES_OPTION) {
-					Album.deleteFromAlbum((String)photoList.get(i).get("filefullname"));
-					Album.deleteFromMemo((String)photoList.get(i).get("filename"));
+				int delete = JOptionPane.showConfirmDialog(null, "정말로 삭제하시겠습니까?", "삭제", JOptionPane.YES_NO_OPTION,
+						JOptionPane.WARNING_MESSAGE);
+				if (delete == JOptionPane.YES_OPTION) {
+					Album.deleteFromAlbum((String) photoList.get(i).get("filefullname"));
+					Album.deleteFromMemo((String) photoList.get(i).get("filename"));
 					Key.deleteKey(photoList, photoList.get(i), "Key");
 					JOptionPane.showMessageDialog(null, "사진이 삭제되었습니다.");
 					GUI.main_panel.removeAll();
@@ -176,7 +178,7 @@ public class PhotoPanel extends JPanel {
 			}
 		});
 		east_panel.add(delete_button);
-		
+
 		/* 이전 화면(모든 사진)으로 돌아감 */
 		JButton back_button = new JButton("이전으로");
 		back_button.addActionListener(new ActionListener() {
@@ -188,7 +190,6 @@ public class PhotoPanel extends JPanel {
 			}
 		});
 		west_panel.add(back_button);
-
 
 	}
 }
