1e5b741f00e58f4cd78cc699103b62549d90ee20
soy-A <skql1502@gmail.com>
2020-12-13 19:30:32 +0900

    chore: Remove unused package

--- src/GUI.java
+++ src/GUI.java
@@ -13,7 +13,6 @@ import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
